36fdc832221f6dc946a35fa1d4db5ffde33aa19a
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-13 12:56:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,13 @@
 def game_hash
-  game_hash = {:home => {},:away => {}}
+  game_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [:player_name => {}]
+    },
+    :away => {
+      :team_name => {},
+      :colors => {},
+      :players => {},
+    }
 end
