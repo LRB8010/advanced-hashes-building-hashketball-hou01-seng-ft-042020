152154fd614e50ea5ff69507fe5d90892be4701b
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-13 13:24:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -52,8 +52,8 @@ def game_hash
       ]
     },
     :away => {
-      :team_name => {},
-      :colors => {},
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
       :players => {},
     }
   }
