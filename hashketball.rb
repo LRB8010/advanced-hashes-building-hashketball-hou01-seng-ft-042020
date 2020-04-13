39a0efc033ab0a4af9d1e5bedc45b48623132d31
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-13 13:14:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -31,7 +31,7 @@ def game_hash
               :steals => 3,
               :blocks => 1,
               :slam_dunks => 15 },
-              {:player_name => "Mason Plimlee",
+              {:player_name => "Mason Plumlee",
                 :number => 1,
                 :shoe => 19,
                 :points => 26,
@@ -39,7 +39,16 @@ def game_hash
                 :assists => 6,
                 :steals => 3,
                 :blocks => 8,
-                :slam_dunks => 5 }
+                :slam_dunks => 5 },
+                {:player_name => "Jason Terry",
+                  :number => 30,
+                  :shoe => 14,
+                  :points => 12,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 12,
+                  :blocks => 12,
+                  :slam_dunks => 7}
       ]
     },
     :away => {
